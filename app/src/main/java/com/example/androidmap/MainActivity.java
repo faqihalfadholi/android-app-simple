bd05a8c26a1e133ded4dce8944f17bffd4acf0b2
Muhammad Faqih Al Fadholi <faqihalfadholi@gmail.com>
2024-11-29 14:30:56 +0700

    Android App

---
+++ app/src/main/java/com/example/androidmap/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.androidmap;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng uiDepok = new LatLng(-6.3656, 106.8267);
+        mMap.addMarker(new MarkerOptions().position(uiDepok).title("Universitas Indonesia, Depok"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uiDepok, 15));
+    }
+}
